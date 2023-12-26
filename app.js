1ac63d92649f51d4c1261fdfc8c1330db0f3b925
michael mbugua <mikembugua960@gmail.com>
2023-12-26 12:30:44 +0300

    create maps function

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-fun;
+let map;
+function initMap() {
+  let options = {
+    center: { lat: 38.346, lng: -0.4907 },
+    zoom: 9,
+  };
+
+  map = new google.maps.Map(document.getElementById("map"), options);
+}

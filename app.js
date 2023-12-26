7ecefd301438a6616f864daf6e13e5c630028bb6
michael mbugua <mikembugua960@gmail.com>
2023-12-26 12:58:01 +0300

    create a marker

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 let map;
 function initMap() {
   let options = {
-    center: { lat: 38.346, lng: -0.4907 },
+    center: { lat: 1.0135, lng: 36.9053 },
     zoom: 9,
   };
 
   map = new google.maps.Map(document.getElementById("map"), options);
+
+  const marker = new google.maps.Marker({
+    position: { lat: 1.0135, lng: 36.9053 },
+    map: map,
+  });
 }
